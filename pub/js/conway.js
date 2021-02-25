2c32e699cd67d05fd3bf4bc9667273ba485e1ebd
ArjunPandher <arjunpandher@gmail.com>
2021-02-24 22:42:47 -0500

    Implemented drawgrid() function

--- pub/js/conway.js
+++ pub/js/conway.js
@@ -1,8 +1,20 @@
 "use strict";
 
+// Current version specifications:
+// Cell pixel dimensions: 20 by 20 px
+// Grid width: 2px
+// Board dimensions: 24 by 24 cells
+// Canvas height/width: 526 by 526 px
 (function(global, document, $){
     function Conway (canvasID) {
         this.canvas = document.getElementById(canvasID)
+        this.ctx = canvas.getContext("2d");
+        // Overriding the canvas's height and width; will support other canvas sizes at a later date.
+        // Maybe create the canvas ourselves?
+        this.canvas.width = 526;
+        this.canvas.height = 526;
+        this.canvas.style.width = 526 + "px";
+        this.canvas.style.height = 526 + "px";
         this.cellheight = 24;
         this.cellwidth = 24;
         this.cellarray = new Array(this.cellheight);
@@ -57,11 +69,33 @@
         // visually updates the board to represent current cell data
         updateboard : function () {
             // TODO: this
+
+            // Draw grid
+
+            //
         },
         // visually updates a single cell to represet that cell's current state
         // TODO: make this private
         updatecell : function (x, y) {
             // TODO: this
+        },
+        // draws a grid on the canvas
+        drawgrid : function () {
+            let gridwidth = 2;
+            let cellwidth = 20;
+            let cellheight = 20;
+            this.ctx.strokeStyle = "#B6E6EC";
+            this.ctx.lineWidth = 2;
+            for (let i = 0; i < this.canvas.height; i += gridwidth + cellwidth) {
+                this.ctx.moveTo(0, i);
+                this.ctx.lineTo(this.canvas.width, i);
+                this.ctx.stroke();
+            }
+            for (let i = 0; i < this.canvas.width; i += gridwidth + cellheight) {
+                this.ctx.moveTo(i, 0);
+                this.ctx.lineTo(i, this.canvas.height);
+                this.ctx.stroke();
+            }
         }
     }
 })(window, window.document, $);
