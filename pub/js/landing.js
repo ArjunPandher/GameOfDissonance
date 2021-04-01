fdbc64c916899d03eafa897025fc3814ea2aa7ed
Peter Bilski <peterbilski@Peters-Air-2.fios-router.home>
2021-03-31 20:26:44 -0400

    erased testing code

--- pub/js/landing.js
+++ pub/js/landing.js
@@ -6,3 +6,7 @@ const noteField = document.getElementById("noteField")
 conway.drawgrid();
 conway.initCanvas();
 
+conway.updatecell(0, 0)
+conway.updatecell(0, 1)
+conway.updatecell(1, 0)
+conway.updatecell(3, 3)
