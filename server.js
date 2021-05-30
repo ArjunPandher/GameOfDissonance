b5f42641a200c49568ec9b7309bac27738ca5f30
ArjunPandher <arjunpandher@gmail.com>
2021-05-30 16:56:29 -0400

    added routing for files that needed it

--- server.js
+++ server.js
@@ -5,12 +5,26 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, '/pub')))
+
 // body-parser: middleware for parsing HTTP JSON body into a usable object
 // const bodyParser = require("body-parser");
 // app.use(bodyParser.json({limit: '50mb'}));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/pub/landing.html'))
+})
+
+app.get('landing.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pub/landing.css'))
+})
+
+app.get('js/conway.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pub/js/conway.js'))
+})
+
+app.get('js/landing.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pub/js/landing.js'))
 })
 
 // will use an 'environmental variable', process.env.PORT, for deployment.
